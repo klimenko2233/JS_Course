f67cb0e4018e82f59ce71af97ca0cf07ad5e2458
aleksadrklimenko <ngk1dnbs@gmail.com>
2025-05-01 12:33:57 +0300

    Дз-1

---
+++ Homeworks/HW-1/main.js
@@ -0,0 +1,47 @@
+// #dYQNrBV
+let a = `hello`
+let b = `owu`
+let c = `com`
+let d = `ua`
+let x= 1
+let y = 10
+let z = -999
+let qwerty= 123
+let pi=3.14
+let smth=2.7
+let sxtn=16
+let t=true
+let f=false
+console.log(a+` `+b+`.`+c+`.`+d)
+console.log(x)
+console.log(y)
+console.log(z)
+console.log(qwerty)
+console.log(pi)
+console.log(smth)
+console.log(sxtn)
+console.log(t)
+console.log(f)
+// #6Qb97gsv ----------------------------------------
+let firstName=`Oleksandr`
+let lastName=`Klimenko`
+let middleName=`Viktorovich`
+let person = lastName+` `+firstName+` `+middleName
+let person2 = `${lastName} ${firstName} ${middleName}`
+console.log(person)
+console.log(person2)
+// #4N0y5tufA ---------------------------------------------
+let A=100
+let B=`100`
+let C=true
+console.log(typeof A)
+console.log(typeof B)
+console.log(typeof C)
+// #ruUtWDUI ------------------------------------------------
+let FirstName= prompt(`Введіть ваше імʼя`)
+let MiddleName=prompt(`Введіть ваше по-батькові`)
+let LastName=prompt(`Введіть ваше прізвище`)
+console.log(`Вітаю вас,`,`${LastName} ${FirstName} ${MiddleName}`)
+
+
+
