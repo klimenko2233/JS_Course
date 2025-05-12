cf1fbdf1f48f2625549a071951647f988713cd59
aleksadrklimenko <ngk1dnbs@gmail.com>
2025-05-12 15:57:07 +0300

    Homework 5

--- Homeworks/HW-5/main.js
+++ Homeworks/HW-5/main.js
@@ -1,55 +1,43 @@
-// #I2XsG6f
-// – створити функцію, яка обчислює та повертає площу прямокутника зі сторонами  а і б
-function square(a, b) {
-    return a * b;
-}
+// виконати задачі за допомоги стрілочних функцій
 
+// #I2XsG6f
+// – створити функцію, яка обчислює та повертає площу прямокутника зі сторонами а і б
+let square = (a, b) => a * b;
 
 // #ETGAxbEn8l
 // – створити функцію, яка обчислює та повертає площу кола з радіусом r
-function circleS(r) {
-    return Math.PI * r * r;
-}
-
+let circleS = (r) => Math.PI * r * r;
 
 // #Mbiz5K4yFe7
 // – створити функцію, яка обчислює та повертає площу циліндру висотою h, та радіусом r
-function cylinder(h, r) {
-    return 2 * Math.PI * r * h + r;
-}
-
+let cylinder = (h, r) => 2 * Math.PI * r * h + r;
 
 // #SIdMd0hQ
 // – створити функцію, яка приймає масив та виводить кожен його елемент
-function element_of_arr(array) {
-    for (const element of array) {
+let element_of_arr = (arr) => {
+    for (let element of arr) {
         console.log(element);
     }
 }
 
-
 // #59g0IsA
 // – створити функцію, яка створює параграф з текстом та виводить його через document.write. Текст задати через аргумент
-function writer(text) {
-    document.write(`<p>${text}</p>`);
-}
-
+let writer = (text) => document.write(`<p>${text}</p>`);
 
 // #hOL6126
-// – створити функцію, яка створює ul з трьома елементами li та виводить його через document.write. Текст li задати через аргумент всім однаковий
-function create_ul(li) {
+// – створити функцію, яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+let create_ul = (li) => {
     document.write(`
     <ul>
         <li>${li}</li>
         <li>${li}</li>
         <li>${li}</li>
-    </ul>`);
+   </ul>`);
 }
 
-
 // #0Kxco1edSN
-// – створити функцію, яка створює ul з  елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл) та виводить його через document.write
-function create_Ul(li, index) {
+// – створити функцію, яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+let create_Ul = (li, index) => {
     document.write(`<ul>`)
     for (let i = 0; i < index; i++) {
         document.write(`<li>${li}</li>`)
@@ -57,10 +45,9 @@ function create_Ul(li, index) {
     document.write(`</ul>`)
 }
 
-
 // #gEFoxMMO
-// – створити функцію, яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список (ul li) та виводить його через document.write
-function ul_elements_of_array(array) {
+// – створити функцію, яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+let ul_elements_of_array = (array) => {
     document.write(`<ul>`)
     for (let i = 0; i < array.length; i++) {
         document.write(`<li>${array[i]}</li>`)
@@ -68,10 +55,9 @@ function ul_elements_of_array(array) {
     document.write(`</ul>`)
 }
 
-
 // #bovDJDTIjt
 // – створити функцію, яка приймає масив об’єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об’єкту окремий блок.
-function objects_writer(array) {
+let objects_writer = (array) => {
     for (let i = 0; i < array.length; i++) {
         document.write(`<div>
         ${array[i].id}.${array[i].name} - ${array[i].age} years old
@@ -79,10 +65,9 @@ function objects_writer(array) {
     }
 }
 
-
 // #pghbnSB
-// – створити функцію яка повертає найменьше число з масиву
-function min_num(array) {
+// – створити функцію, яка повертає найменше число з масиву
+let min_num = (array) => {
     let result=array[0];
     for (let i=1 ;i<array.length;i++) {
         if (array[i]<result) {
@@ -92,11 +77,9 @@ function min_num(array) {
     return result;
 }
 
-
-
 // #EKRNVPM
 // – створити функцію sum(arr), яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
-function sum(array) {
+let sum = (array) => {
     let result=0;
     for (const arrayElement of array) {
         result += arrayElement;
@@ -104,18 +87,22 @@ function sum(array) {
     return result;
 }
 
-
 // #kpsbSQCt2Lf
 // – створити функцію swap(arr,index1,index2). Функція міняє місцями значення у відповідних індексах
-// Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
-function swap(arr,index1,index2) {
+let swap = (arr,index1,index2) => {
     let x=arr[index1];
     arr[index1]=arr[index2];
     arr[index2]=x;
     return arr;
 }
 
-
-
-
-
+// – Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
+// Приклад exchange(10000,[{currency:’USD’,value:40},{currency:’EUR’,value:42}],’USD’) // => 250
+let exchange = (sumUAH, currencyValues, exchangeCurrency) => {
+    for (const element of currencyValues) {
+        if (element.currency === exchangeCurrency) {
+            return  sumUAH/element.value;
+        }
+    }
+    return -Infinity;
+}
